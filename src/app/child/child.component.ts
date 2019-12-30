c4f8b14ba549fa4d9f40cc50f03b8a84556fb3d0
kiranlasure <kiranlasure95@gmail.com>
2019-12-30 11:51:56 +0530

    form UI complete

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 
+  topics = ["Angular","View","React"];
   constructor() { }
 
   ngOnInit() {
