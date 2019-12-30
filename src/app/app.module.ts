a7f93af092a9c90f97c4ec826702d5dda9a4a66a
kiranlasure <kiranlasure95@gmail.com>
2019-12-30 11:42:19 +0530

    form creation started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { ChildcomponentComponent } from './childcomponent/childcomponent.component';
+
 import { ChildComponent } from './child/child.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ChildcomponentComponent, ChildComponent ],
+  declarations: [ AppComponent, HelloComponent,  ChildComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
